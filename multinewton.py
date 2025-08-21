562cae20814d140c17781eb910cad9c13bb04306
Your Name <your_name@berkeley.edu>
2025-08-21 10:51:15 -0700

    multivariate newton code

---
+++ multinewton.py
@@ -0,0 +1,15 @@
+#multivariate newton
+import numpy as np
+from scipy.linalg import solve
+def multinewton(x0, f, maxiter=100, e=1e-5):
+    x=np.array(x0, datatype=float)
+    for i in range(maxiter):
+        g= first_der(f, x, eps)
+        h=second_der(f, x, eps)
+    gradhess= solve(h, g)
+    xnew= x-gradhess
+    if np.linalg.norm (xnew-x) < tol:
+        return xnew
+    x=xnew 
+    return x
+        
